34d173ae2bc9ab42809bedef1d6976513b2d56a3
Maija Grimes <maija.grimes@colorado.edu>
2026-03-01 15:32:53 -0700

    new movement features

--- resources/script.js
+++ resources/script.js
@@ -1,4 +1,4 @@
-const openbutton = document.getElementById('openbutton');
+/*const openbutton = document.getElementById('openbutton');
 const navbar = document.getElementById('navbar');
 const mainContent = document.getElementById('cover');
 const drawerSound = new Audio('./resources/sounds/drawer-open.mp3');
@@ -15,4 +15,29 @@ openbutton.addEventListener('click', () => {
     } else {
         mainContent.style.transform = 'translateY(0)';
     }
+});*/
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    const page1 = document.getElementById('page1');
+    const page2 = document.getElementById('page2');
+
+    console.log(page1, page2);
+
+    page2.addEventListener('click', () => {
+        page2.style.zIndex = "8";
+        page1.style.zIndex = "1";
+
+        page2.classList.add("active");
+        page1.classList.remove("active");
+    });
+
+    page1.addEventListener('click', () => {
+        page1.style.zIndex = "5";
+        page2.style.zIndex = "1";
+
+        page1.classList.add("active");
+        page2.classList.remove("active");
+    });
+
 });
