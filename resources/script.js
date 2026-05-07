4dc45bf41ec11fbb097f4659458f589a0e75499d
Maija Grimes <maija.grimes@colorado.edu>
2026-05-07 16:49:53 -0600

    new photo stack

--- resources/script.js
+++ resources/script.js
@@ -41,3 +41,48 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 
 });
+
+//Get all image container
+let childElement = document.querySelectorAll(".image-container");
+
+//Array of image URLs
+let imageUrls = ["./resources/images/meandkathryn.png", "./resources/images/boulderhiking.png", "./resources/images/poncho.png", "./resources/images/football.png", "./resources/images/jinx.png"];
+
+//Initial z-index value
+let zIndex = 1;
+
+//Iterate through each image container
+childElement.forEach((element, index) => {
+    //Create an image element
+    let img = document.createElement("img");
+    img.setAttribute("src", imageUrls[index]);
+    img.setAttribute("class", "image");
+    element.appendChild(img);
+
+    //Add a click event listener to each image container
+    element.addEventListener("click", () => {
+    zIndex++;
+    //element.style.right = "45em";
+    element.style.zIndex = zIndex.toString();
+    element.style.transform = "rotate(0deg)";
+
+    //Apply transition to right property
+    //element.style.transition = " right 0.2s ease";
+
+    //Remomve the class and reset properties after a delay
+    //setTimeout(() => {
+        zIndex -= 3;
+        element.style.right = "";
+        element.style.zIndex = zIndex.toString();
+        element.style.transform = "";
+
+      //Set a transition for clearing the transition property
+        //element.style.transition = "all 0.3s ease";
+
+      //Clear the transition property after a slight delay
+        //setTimeout(() => {
+        //element.style.transition = "";
+        //, 300);
+    //}, 1000);
+    });
+});
